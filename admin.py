7390679ecdcad610fdf2a726afba48bfe24ba267
liginity <liginity@outlook.com>
2021-01-05 21:39:27 +0800

    (split) register the models in admin site
    now adding answers works.

--- admin.py
+++ admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from askaboutme.models import SimpleQuestion, PublicAnswer
+
+
+@admin.register(SimpleQuestion)
+class SimpleQuestionAdmin(admin.ModelAdmin):
+    fields = [
+        "content",
+        "created_time",
+        "public_answer",
+    ]
+
+
+@admin.register(PublicAnswer)
+class PublicAnswerAdmin(admin.ModelAdmin):
+    fields = [
+        "content",
+        "answered_time",
+    ]
