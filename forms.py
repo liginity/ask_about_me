0bf463ee787ebbca62124c2ffdefdccf86638d59
liginity <liginity@outlook.com>
2021-01-05 20:29:09 +0800

    (split) add a captcha to prevent bot
    though I choose to make captcha easy to answer by disabling all the noise in the
    image.

--- forms.py
+++ forms.py
@@ -1,7 +1,9 @@
 from django import forms
+from captcha.fields import CaptchaField
 
 
 class SimpleQuestionForm(forms.Form):
     """A form for simple questions."""
 
     qcontent = forms.CharField(max_length=700,label="Question:" , help_text="Write a question to ask.", widget=forms.Textarea)
+    captcha = CaptchaField()
