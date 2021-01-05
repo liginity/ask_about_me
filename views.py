57957612c0db5b578535ec562e01f2cb97e4b505
liginity <liginity@outlook.com>
2021-01-06 01:23:10 +0800

    (split) display question numbers

--- views.py
+++ views.py
@@ -6,9 +6,16 @@ from django.urls import reverse
 from askaboutme.models import SimpleQuestion
 from askaboutme.forms import SimpleQuestionForm
 
+
 def index(request):
-    question_list = SimpleQuestion.objects.filter(polite_q=True, wont_be_answered_q=False).order_by("-created_time")
-    return render(request, "askaboutme/index.html", context={"question_list": question_list,})
+    question_list = SimpleQuestion.objects.filter(
+        polite_q=True, wont_be_answered_q=False
+    ).order_by("-created_time")
+    context = {
+        "question_list": question_list,
+        "polite_question_number": question_list.count(),
+    }
+    return render(request, "askaboutme/index.html", context=context)
 
 
 def ask_a_question(request):
